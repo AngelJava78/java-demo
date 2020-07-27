5a414c426af5190f774c46ae8096aaefdf353088
AngelJava78 <angel_java@hotmail.com>
2020-07-26 21:28:33 -0500

    Commit inicial

---
+++ src/app/java/AppJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package app.java;
+
+/**
+ *
+ * @author AJAVIER
+ */
+public class AppJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!!!");
+        System.out.println("Press any key to continue...");
+    }
+    
+}
